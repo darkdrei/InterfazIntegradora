0d4f2854c9ce017d1b45397bed351fc902c606a4
darkdrei <darkdrei88@gmail.com>
2017-08-27 21:22:21 -0500

    Esta interfaz que contiene la  validacion y la existencia en los archivos a analizar

---
+++ Core/src/core/Validacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.io.File;
+
+/**
+ *
+ * @author dark
+ */
+public interface Validacion {
+    public boolean exisFile(String ruta);
+    public boolean validExtencion(File f);
+    public boolean validExtencion(String f);
+}
