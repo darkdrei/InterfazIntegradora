c52f837529615fc46a0b7b9c4cc9ccd002f464a1
darkdrei <darkdrei88@gmail.com>
2017-08-27 21:17:38 -0500

    esta interfaz contiene los metodos de lectura y escritura del archivo

---
+++ Core/src/core/OperacionFIle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.io.File;
+
+/**
+ *
+ * @author dark
+ */
+public interface OperacionFIle {
+    public File loadFile(String ruta);
+    public void loadingFile(String ruta);
+    public void readNodeFile(File f);
+    public void readNodeFile();
+    public void extraerInformacion();
+    public void writeFile();
+    public void writeFile(String ruta);
+    public void writeFile(String ruta, Xml xml);
+        
+}
