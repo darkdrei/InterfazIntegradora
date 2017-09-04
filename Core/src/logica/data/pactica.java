95f315e523c6f81f3d90c121b9e3d566560a7340
darkdrei <darkdrei88@gmail.com>
2017-09-04 01:28:34 -0500

    registro de modi e configuraciones

--- Core/src/logica/data/pactica.java
+++ Core/src/logica/data/pactica.java
@@ -5,6 +5,7 @@
  */
 package logica.data;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -19,35 +20,36 @@ public class pactica {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Vector<Integer> t = new Vector<>();
-        t.add(1);
-        t.add(1);
-        t.add(1);
-        t.add(1);
-        t.add(1);
-        t.add(1);
-        t.add(1);
-        Object v[] = getvector(3);
-        System.err.println(Arrays.asList(t).get(0).toArray());
-        if (v instanceof String[]){
-            System.err.println("Es una cadena "+((String[])v));
-            String r[] = (String[])v;
-            for (String r1 : r) {
-                System.err.println(r1);
-            }
-        }else if (v instanceof Integer[]){
-            System.err.println("Es un entero"+((Integer[])v));
-            Integer r[] = (Integer[])v;
-            for (int r1 : r) {
-                System.err.println(r1);
-            }
-        }else if (v instanceof Boolean[]){
-            System.err.println("Es una boleano  "+((Boolean[])v));
-            Boolean r[] = (Boolean[])v;
-            for (boolean r1 : r) {
-                System.err.println(r1);
-            }
-        }
+        ZipUtils.extract(new File("/home/dark/proyectos/Modulos_test/Modulo1.zip") , new File("/home/dark/Escritorio/Desde-Java"));
+//        Vector<Integer> t = new Vector<>();
+//        t.add(1);
+//        t.add(1);
+//        t.add(1);
+//        t.add(1);
+//        t.add(1);
+//        t.add(1);
+//        t.add(1);
+//        Object v[] = getvector(3);
+//        System.err.println(Arrays.asList(t).get(0).toArray());
+//        if (v instanceof String[]){
+//            System.err.println("Es una cadena "+((String[])v));
+//            String r[] = (String[])v;
+//            for (String r1 : r) {
+//                System.err.println(r1);
+//            }
+//        }else if (v instanceof Integer[]){
+//            System.err.println("Es un entero"+((Integer[])v));
+//            Integer r[] = (Integer[])v;
+//            for (int r1 : r) {
+//                System.err.println(r1);
+//            }
+//        }else if (v instanceof Boolean[]){
+//            System.err.println("Es una boleano  "+((Boolean[])v));
+//            Boolean r[] = (Boolean[])v;
+//            for (boolean r1 : r) {
+//                System.err.println(r1);
+//            }
+//        }
     }
     
     
