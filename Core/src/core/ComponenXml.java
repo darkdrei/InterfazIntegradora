f22bcf7ffa2d5f4f00b9dd17705c535b940ed99d
darkdrei <darkdrei88@gmail.com>
2017-09-10 22:59:36 -0500

    se agregan el metodo de escritura de archivo xml

--- Core/src/core/ComponenXml.java
+++ Core/src/core/ComponenXml.java
@@ -113,4 +113,11 @@ public class ComponenXml implements OperacionFIle{
     public void readFile(Xml xml) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public String toString() {
+        return xml.toString();
+    }
+    
+    
 }
